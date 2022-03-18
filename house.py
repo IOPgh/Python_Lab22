34f1e0d6c6005308dced2f3bd25e02717a1429b4
IOPgh <iop.yandex@gmail.com>
2022-03-18 16:47:16 +0300

    Add main

---
+++ house.py
@@ -0,0 +1,22 @@
+def main():
+    x,y = 100, 100
+    width, height = 200, 200
+    draw_house(x,y, width, height)
+
+
+def draw_house(x,y, width, height):
+    """
+
+    :param x:
+    :param y:
+    :param width:
+    :param height:
+    :return:
+    """
+    print("Paint house...",x,y,width, height)
+    pass
+
+
+main()
+
+
