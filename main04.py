11836ca0dd5506bccd2d13d2d69a5ad2e106925c
IOPgh <iop.yandex@gmail.com>
2022-03-18 16:33:51 +0300

    Add draw_house

--- main04.py
+++ main04.py
@@ -1,1 +1,17 @@
-print("Lab04")
+def draw_house(x,y, width, height):
+    """
+
+    :param x:
+    :param y:
+    :param width:
+    :param height:
+    :return:
+    """
+    print("Paint house...",x,y,width, height)
+    pass
+
+x,y = 100, 100
+width, height = 200, 200
+
+
+draw_house(x,y, width, height)
